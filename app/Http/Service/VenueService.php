c4d617f254cfcf2eb3b618437b116181877811cf
badou <>
2026-01-11 14:32:16 +0800

    encode

--- app/Http/Service/VenueService.php
+++ app/Http/Service/VenueService.php
@@ -122,7 +122,7 @@ class VenueService{
             'end_time' => strtotime($request['end_time']),
             'venue_introduction' => $data['venue_introduction'] ?? '',
             'labels' => $request['labels'],
-            'venue_config' => json_encode($venueConfig),
+            'venue_config' => json_encode($venueConfig,true),
             'support_status' => 2,
         ];
 
@@ -164,6 +164,7 @@ class VenueService{
         $list['end_time'] = date('H:i',$list['end_time']);
         $list['venue_image'] = explode(',',$list['venue_image']);
         $venue_config = json_decode($list['venue_config'],true);
+        unset($list['venue_config']);
         $list['one_billing'] = $venue_config['one_billing'];
         $list['time_billing'] = $venue_config['time_billing'];
         $vehicle = Vehicle::select('id','vehicle_name','vehicle_introduction','top_speed','vehicle_image','vehicle_state','is_password','vehicle_battery')->where(['agent_id'=>$data['agent_id'],'venue_id'=>$list['id']])->get(); //车辆列表
