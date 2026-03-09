2d35d81d18850e65c5dbe9cb04702120e801d011
badou <>
2026-03-09 14:32:54 +0800

    1

--- app/Http/Service/VehicleService.php
+++ app/Http/Service/VehicleService.php
@@ -516,7 +516,7 @@ class VehicleService
                 'change_ui_control'=>$change_ui_control,]);
         } elseif(isset($request['password'])) {
             Vehicle::where('id', $id)->update(['password' => '',
-                'is_password'=>1,
+                'is_password'=>0,
                 'reverse_left_right'=>$reverse_left_right,
                 'reverse_up_down'=>$reverse_up_down,
                 'reverse_rotation'=>$reverse_rotation,
