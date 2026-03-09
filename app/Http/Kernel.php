f0d89e592e04796ba6f391f92a63fb8f4cb9f5cf
badou <>
2026-03-09 16:11:42 +0800

    去除

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,7 +64,7 @@ class Kernel extends HttpKernel
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'precognitive' => \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
         'signed' => \App\Http\Middleware\ValidateSignature::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+//        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'checkAesEntry' => \App\Http\Middleware\CheckAesEntry::class,
         'AuthToken' => \App\Http\Middleware\AuthToken::class,
