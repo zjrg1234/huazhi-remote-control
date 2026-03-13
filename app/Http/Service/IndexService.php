14c179d91ef5cea460a0912abd76c3b3e44ea60e
badou <>
2026-03-13 16:42:56 +0800

    1

--- app/Http/Service/IndexService.php
+++ app/Http/Service/IndexService.php
@@ -150,7 +150,7 @@ class IndexService{
         $list['end_time'] = date('H:i',$list['end_time']);
         $vehicle = Vehicle::select('id','vehicle_name','vehicle_introduction','top_speed','vehicle_image','vehicle_state','is_password','vehicle_battery','password','app_transmitter_id','status')->where(['agent_id'=>$list['agent_id'],'venue_id'=>$list['id'],'status'=>1])->get(); //车辆列表
         foreach($vehicle as $value){
-            $vehicle_people_number = DrivingRecord::where('vehicle_id', $value['id'])->where('reservation_status', 1)->count();//表未建立 暂定
+            $vehicle_people_number = DrivingRecord::where('vehicle_id', $value['id'])->whereIn('reservation_status', [1,2,3])->count();//表未建立 暂定
             $value['vehicle_queue'] = $vehicle_people_number ?? 0;
         }
         $list['venue_config'] = json_decode($list['venue_config'],true);
