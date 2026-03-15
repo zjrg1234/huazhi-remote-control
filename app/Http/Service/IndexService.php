39ca99c0f872221bc256d4864de245a020f958b4
badou <>
2026-03-15 18:12:30 +0800

    11

--- app/Http/Service/IndexService.php
+++ app/Http/Service/IndexService.php
@@ -62,9 +62,9 @@ class IndexService{
         }
         $cuserAgentId = CuserAgent::where('superior_agent_id',$user['special_area'])->pluck('id');
         if($type != 0){
-            $venueList = AgentVenue::select('id','venue_name','venue_image','vehicle_id')->whereIn('agent_id',$cuserAgentId)->where('vehicle_id',$type)->get();
+            $venueList = AgentVenue::select('id','venue_name','venue_image','vehicle_id')->whereIn('agent_id',$cuserAgentId)->where('vehicle_id',$type)->where('support_status',1)->get();
         }else{
-            $venueList = AgentVenue::select('id','venue_name','venue_image','vehicle_id')->whereIn('agent_id',$cuserAgentId)->get();
+            $venueList = AgentVenue::select('id','venue_name','venue_image','vehicle_id')->whereIn('agent_id',$cuserAgentId)->where('support_status',1)->get();
         }
         $redisKey = $user['special_area'].'_type_'.$type;
         $redis = Redis::get($redisKey);
