648eafb9203600812245ad8bcc6fc2dffbd9e5f8
badou <>
2026-05-16 16:54:36 +0800

    ios

--- app/Http/Service/IndexService.php
+++ app/Http/Service/IndexService.php
@@ -215,7 +215,7 @@ class IndexService{
         if(isset($type) && $type == 2){
             $specialList = CuserAgent::select('id','agent_name','head_shot','type')->whereIn('type',[2,3])->where('superior_agent_id',0)->get();
         }else{
-            $specialList = CuserAgent::select('id','agent_name','head_shot','type')->where('type',3)->where('superior_agent_id',0)->get();
+            $specialList = CuserAgent::select('id','agent_name','head_shot','type')->whereIn('type',[1,3])->where('superior_agent_id',0)->get();
         }
         $sid = $specialList->pluck('id');
         $amountArray = CuserWallet::where('uid',$uid)->whereIn('type',$sid)->pluck('balance','type')->toArray();
