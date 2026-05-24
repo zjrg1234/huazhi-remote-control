283d3cfa367e29574fbb22e14926c9f95788301d
badou <>
2026-05-24 16:02:43 +0800

    token校验

--- app/Http/Middleware/CheckToken.php
+++ app/Http/Middleware/CheckToken.php
@@ -41,22 +41,25 @@ class CheckToken
             if(!$user) {
                 return ReponseData::reponseFormat(401, '未找到该用户!');
             }
+            $userToken = Redis::get($key);
+
+            if ($userToken != $session_key) {
+                return ReponseData::reponseFormat(401, 'token 验证错误!');
+            }
         }else if($agent_id){
             $key = 'agent_token_'.$request['agent_id'];
             $user = CuserAgent::find($agent_id);
             if(!$user) {
                 return ReponseData::reponseFormat(401, '未找到该用户!');
             }
+            $userToken = Redis::get($key);
         }else{
             return ReponseData::reponseFormat(401, '请先登陆!');
         }
-        $userToken = Redis::get($key);
         if (!$userToken) {
             return ReponseData::reponseFormat(401, '请先登陆!');
         }
-        if ($userToken != $session_key) {
-            return ReponseData::reponseFormat(401, 'token 验证错误!');
-        }
+
 
 //        if($user['is_locket'] == 1){
 //            return ReponseData::reponseFormat(130, '该账户已被锁定，请联系管理员!');
