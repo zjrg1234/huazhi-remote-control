e13ebc47dc89ac526161e10c2d9d9bdb317173ed
badou <>
2026-05-24 15:03:15 +0800

    1

--- app/Http/Service/AgentService.php
+++ app/Http/Service/AgentService.php
@@ -229,7 +229,7 @@ class AgentService
     {
         $data = [
             'level' => $request['level'] ?? null, //代理商等级
-            'superior_agent_id' => $request['superior_agent_id'] ?? null, //上级代理商id
+            'superior_agent_id' => $request['superior_agent_id'] ?? 0, //上级代理商id
             'phone_number'=>$request['phone_number'] ?? null, //手机号
             'head_shot' => $request['head_shot'] ?? 'https://zksj-new.oss-cn-beijing.aliyuncs.com/zk/image/ZKSJ_1770280030SR25.jpeg', //头像
             'create_site_quantity' => $request['create_site_quantity'] ?? null, //可创建场地数量
