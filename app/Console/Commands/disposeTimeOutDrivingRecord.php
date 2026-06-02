d39bf9eacab707d9eef2976933376023b8d5b6b5
badou <>
2026-06-02 13:52:14 +0800

    释放

--- app/Console/Commands/disposeTimeOutDrivingRecord.php
+++ app/Console/Commands/disposeTimeOutDrivingRecord.php
@@ -61,7 +61,7 @@ class disposeTimeOutDrivingRecord extends Command
                 $rulesTime = $billing_rules['time'] * 60;
                 $orderAmount = $drivingRecord['payment_amount'];
                 $startTime = $drivingRecord['start_time'];
-                if($startTime === 0){ //如果没有开始驾驶时间 脏的单子 直接取消
+                if($startTime === 0 && $drivingRecord['reservation_status'] == 3){ //如果没有开始驾驶时间 脏的单子 直接取消
                     $drivingRecord->update([
                         'reservation_status' => 5,
                         'transmitter_id' => '0',//释放发射机id
@@ -106,7 +106,7 @@ class disposeTimeOutDrivingRecord extends Command
             if($drivingRecord['billing_method'] == 1){
                 $rulesTime = $billing_rules['time'] * 60;
                 $startTime = $drivingRecord['start_time'];
-                if($startTime === 0){ //如果没有开始驾驶时间 脏的单子 直接取消
+                if($startTime === 0 && $drivingRecord['reservation_status'] == 3){ //如果没有开始驾驶时间 脏的单子 直接取消
                     $drivingRecord->update([
                         'reservation_status' => 5,
                         'transmitter_id' => '0',//释放发射机id
