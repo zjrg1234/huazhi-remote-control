10b058e3e15b948549501f0fd9e67e2456dd471f
badou <>
2026-07-02 09:08:12 +0800

    强制更新

--- app/Http/Middleware/CheckToken.php
+++ app/Http/Middleware/CheckToken.php
@@ -40,22 +40,22 @@ class CheckToken
         $platform = $request->header('platform') ?? '';
         $request_version = $request->header('versionCode') ?? '';
 
-//        if($platform && $platform == 'ZZSJ_i0S'){
-//            $app_version = AppVersion::where(['type'=>1,'status'=>1])->first();
-//            if($app_version['forced_updating'] == 1 && $request_version != $app_version['version_mark']){
-//                return ReponseData::reponseFormat(2000,'请更新最新版app哦!');
-//            }
-//        }
-//        if ($platform && $platform == 'ZZSJ_Android'){
-//            $app_version = AppVersion::where(['type'=>2,'status'=>1])->first();
-//            if($app_version['forced_updating'] == 1 && $request_version != $app_version['version_mark']){
-//                return ReponseData::reponseFormat(2000,'请更新最新版app哦!');
-//            }
-//        }
-//
-//        if($request->server('REQUEST_URI') == '/api/user/start/driving' || $request->server('REQUEST_URI') == '/api/user/reservation'){
-//            return ReponseData::reponseFormat(2000,'请更新最新版app哦!');
-//        }
+        if($platform && $platform == 'ZZSJ_iOS'){
+            $app_version = AppVersion::where(['type'=>1,'status'=>1])->first();
+            if($app_version['forced_updating'] == 1 && $request_version != $app_version['version_mark']){
+                return ReponseData::reponseFormat(2000,'请更新最新版app哦!');
+            }
+        }
+        if ($platform && $platform == 'ZZSJ_Android'){
+            $app_version = AppVersion::where(['type'=>2,'status'=>1])->first();
+            if($app_version['forced_updating'] == 1 && $request_version != $app_version['version_mark']){
+                return ReponseData::reponseFormat(2000,'请更新最新版app哦!');
+            }
+        }
+
+        if($request->server('REQUEST_URI') == '/api/user/start/driving' || $request->server('REQUEST_URI') == '/api/user/reservation'){
+            return ReponseData::reponseFormat(2000,'请更新最新版app哦!');
+        }
         if (!isset($session_key)) {
             return ReponseData::reponseFormat(401, '登录失效!');
         }
