bc37e99504ed9a8847757889f0a1ab7184edda1f
badou <>
2026-09-09 14:25:14 +0800

    注销

--- app/Http/Service/LoginService.php
+++ app/Http/Service/LoginService.php
@@ -47,21 +47,22 @@ class LoginService
                 return ReponseData::reponseFormat(2003,'账号未注册，请先注册哦！');
             }
             if($userInfo['is_cancel'] == 1){
-                $userInfo['is_cancel'] = 0;
-                $nowTime                 = time();
-                $sessionKey              = base64_encode(md5($userInfo['id'].$userInfo['user_name'].$nowTime));
-                $key = 'token_'.$userInfo['id'];
-                $userInfo->save();
-                Redis::set($key, $sessionKey);
-                $response =  [
-                    'id' => $userInfo['id'],
-                    'special_area' => $userInfo['special_area'],
-                    'session_key' => $sessionKey,
-                    'new_user' => 0,
-
-                ];
-                $responseData = $response;
-                return ReponseData::reponseFormatList(200,'成功',$responseData);
+                return ReponseData::reponseFormat(2000,'账号已经注销!');
+//                $userInfo['is_cancel'] = 0;
+//                $nowTime                 = time();
+//                $sessionKey              = base64_encode(md5($userInfo['id'].$userInfo['user_name'].$nowTime));
+//                $key = 'token_'.$userInfo['id'];
+//                $userInfo->save();
+//                Redis::set($key, $sessionKey);
+//                $response =  [
+//                    'id' => $userInfo['id'],
+//                    'special_area' => $userInfo['special_area'],
+//                    'session_key' => $sessionKey,
+//                    'new_user' => 0,
+//
+//                ];
+//                $responseData = $response;
+//                return ReponseData::reponseFormatList(200,'成功',$responseData);
 
             }
             if($userInfo['is_locked'] == 1){
